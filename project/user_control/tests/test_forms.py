71299d03155000e677ffbd8de0f1679d46f92d50
KhandakarMubarsharUddin <86358677+KhandakarMubarsharUddin@users.noreply.github.com>
2021-09-02 23:13:04 +0600

    Create test_forms.py

---
+++ project/user_control/tests/test_forms.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+from user_control.forms import *
+
+
+class TestForms(TestCase):
+    doctor_valid_data = {
+        'email': 'doctor@gmail.com',
+        'name': 'Doctor',
+        'password1': 'asdf123ASDF',
+        'password2': 'asdf123ASDF',
+        'check': True,
+    }
+    doctor_invalid_data = {
+        'email': 'doctor@gmail.com',
+        'name': '',
+        'password1': 'asdf123ASDF',
+        'password2': 'asdf123ASDF',
+        'check': True,
+    }
+    patient_valid_data = {
+        'email': 'patient@gmail.com',
+        'name': 'Patient',
+        'password1': 'asdf123ASDF',
+        'password2': 'asdf123ASDF',
+        'check': True,
+    }
+    patient_invalid_data = {
+        'email': 'patient@.com',
+        'name': '',
+        'password1': 'asdf123ASDF',
+        'password2': 'asdf123ASDF',
+        'check': True,
+    }
+
+    def test_doctor_registration_form_with_valid_data(self):
+        form = DoctorRegistrationForm(data=self.doctor_valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_doctor_registration_form_with_invalid_data(self):
+        form = DoctorRegistrationForm(data=self.doctor_invalid_data)
+        self.assertFalse(form.is_valid())
+
+    def test_patient_registration_form_with_valid_data(self):
+        form = PatientRegistrationForm(data=self.patient_valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_patient_registration_form_with_invalid_data(self):
+        form = PatientRegistrationForm(data=self.patient_invalid_data)
+        self.assertFalse(form.is_valid())
